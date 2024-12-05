d758fef61018a20164aff84a690a7b2ed40f41f7
emholmgren <150334518+emholmgren@users.noreply.github.com>
2024-12-05 10:00:35 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Test Project</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Trying out Netlify!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Web app under construction ...
         </p>
       </main>
 
